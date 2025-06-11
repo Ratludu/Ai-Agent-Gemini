33644836fd609f968e5bad0022ad654cf5d070da
Brandon S <119836270+Ratludu@users.noreply.github.com>
2025-06-11 19:52:05 +1000

    added tests and get files list

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,41 +4,33 @@ def is_within(parent, child):
 
     abs_parent = os.path.abspath(parent) 
     abs_child = os.path.abspath(child) 
-    print(abs_parent, abs_child)
-    return os.path.commonpath([abs_parent, abs_child]) == abs_parent 
+    return abs_child.startswith(abs_parent)
+
 
 def get_files_info(working_directory,directory=None):
 
     # check if the working directory exists 
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
+    new_directory = os.path.join(os.path.abspath(working_directory),directory)
+
+    if not os.path.isdir(new_directory):
+        return f'Error: "{new_directory}" is not a directory'
 
 
-    if not is_within(working_directory, directory):
-        return f"Error: Cannot list {directory} as it is outside the permitted working directoy"
+    if not is_within(working_directory, new_directory):
+        return f"Error: cannot list {new_directory} as it is outside the permitted working directory"
 
-
-
-
-    #loop through the files in the directory
     try:
-        files_info = []
-
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                file_path = os.path.join(root, file)
-                file_dict = {
-                    "name": file,
-                    "file_size": os.path.getsize(file_path),
-                    "is_dir": os.path.isdir(file_path),
-                }
+        files = os.listdir(new_directory)
+        
         result = ""
-        for item in files_info:
-            result += f"- {item['name']}: file_size={item['file_size']} bytes, is_dir={item['is_dir']}\n"
+        for file in files: 
+            file_path = os.path.join(new_directory,file)
+            result += f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)} \n"
+        return result
+    except:
+        return f'Error: "{new_directory}" is not a directory'
 
-        return result 
-    except Exception as e:
-        return f"Error: {str(e)} while processing the directory {directory}."
+
 
 
 
--- tests.py
+++ tests.py
@@ -1,3 +1,14 @@
 from functions.get_files_info import get_files_info 
 
-print(get_files_info("calculator", "."))
+
+tests = {
+
+    "test_1":("calculator","."),
+    "test_2":("calculator","pkg"),
+    "test_3":("calculator","/bin"),
+    "test_4":("calculator","../")
+}
+
+
+for name, params in tests.items():
+    print(get_files_info(params[0], params[1]))
